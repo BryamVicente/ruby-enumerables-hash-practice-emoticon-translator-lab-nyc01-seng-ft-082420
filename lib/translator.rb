5e256b7fdf3b7936c35e45d52fbf52b30baa1a5c
Bryam Vicente <vicentebryam@gmail.com>
2020-08-21 03:00:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -51,7 +51,7 @@ def load_library (file_path)
       }
    }
   emojis.each do |meaning_key, emoticons_value|
-    new_hash = {meaning_key }
+    
     # (translator_hash[:get_meaning])[emoticons_value[1]] = meaning_key
     # (translator_hash[:get_emoticon])[emoticons_value[0]] = emoticons_value[1]
   end
